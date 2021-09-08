3bee95865757c312caa0fbdb205e56291a30fd7f
Győző Vidor <gyozovidor@gmail.com>
2021-09-08 21:04:51 +0200

    Initial commit of all files.

---
+++ src/main/java/com/pacsapp/pacsapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.pacsapp.pacsapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/ex-module-info.java
@@ -0,0 +1,12 @@
+/*module com.pacsapp.pacsapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.pacsapp.pacsapp to javafx.fxml;
+    exports com.pacsapp.pacsapp;
+}*/
